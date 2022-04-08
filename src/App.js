730689eacdab3c15e7657a89412cbbeb766a70fd
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-04-08 09:10:57 +0530

    day-2 part-1 Assignment

--- src/App.js
+++ src/App.js
@@ -1,79 +1,82 @@
 // --------------------------------------------Day-1 Assignment (06/04/2022)----------------------------------------------------------------//
 
-import './App.css';
-import ContactUs from './day1/Buttons-Part-1/ContactUs';
-import Download from './day1/Buttons-Part-1/Download';
-import Help from './day1/Buttons-Part-1/Help';
-import Home from './day1/Buttons-Part-1/Home';
-import JoinUs from './day1/Buttons-Part-1/JoinUs';
-import Login from './day1/Buttons-Part-1/Login';
-import Search from './day1/Buttons-Part-1/Search';
-import Settings from './day1/Buttons-Part-1/Settings';
-import PropsButtonMethod from './day1/Buttons-Part-2/PropsButtonMethod';
-import Mobile from './day1/Display-List-Problem-1/Mobile';
-import Tv from './day1/Display-List-Problem-1/Tv';
-
-function App() {
-  return (
-    <div>
-                                 {/* ------------Part-1------------------- */}
-      <h1  style={{textAlign: "center"}}>Problem - 1</h1>
-      <div className="App">
-        <Mobile/>
-        <Tv/>
-      </div>
-
-
-                            {/* ------------Part-2 (Sub Part-1)------------------- */}
-      <div>
-          <h1  style={{textAlign: "center"}}>Problem - 2 (Method - 1)</h1>
-          <div className="BtnColourDimention">
-            <JoinUs />
-            <Settings />
-          </div>
-          <div className="BtnColourDimention">
-            <Login />
-            <ContactUs />
-          </div>
-          <div className="BtnColourDimention">
-            <Search />
-            <Help />
-          </div>
-          <div className="BtnColourDimention">
-            <Home />
-            <Download />
-          </div>
-      </div>
-
-
-                            {/* ------------Part-2 (Sub Part-2)------------------- */}
-      <div>
-            <h1  style={{textAlign: "center"}}>Problem - 1 (Method -  2)</h1>
-            <div className='BtnColourDimention2'>
-                <PropsButtonMethod/>
-            </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
-
-
-//------------------------------------------- Day-2 Lecture Part (07/04/2022)-------------------------------------------------------------------//
-
 // import './App.css';
-
-// import Button from './day2/Button';
-// import Employ from './day2/Employ';
+// import ContactUs from './day1/Buttons-Part-1/ContactUs';
+// import Download from './day1/Buttons-Part-1/Download';
+// import Help from './day1/Buttons-Part-1/Help';
+// import Home from './day1/Buttons-Part-1/Home';
+// import JoinUs from './day1/Buttons-Part-1/JoinUs';
+// import Login from './day1/Buttons-Part-1/Login';
+// import Search from './day1/Buttons-Part-1/Search';
+// import Settings from './day1/Buttons-Part-1/Settings';
+// import PropsButtonMethod from './day1/Buttons-Part-2/PropsButtonMethod';
+// import Mobile from './day1/Display-List-Problem-1/Mobile';
+// import Tv from './day1/Display-List-Problem-1/Tv';
 
 // function App() {
 //   return (
-//     <div className="App">
-//       <Button/>
-//       <Employ/>
+//     <div>
+//                                  {/* ------------Part-1------------------- */}
+//       <h1  style={{textAlign: "center"}}>Problem - 1</h1>
+//       <div className="App">
+//         <Mobile/>
+//         <Tv/>
+//       </div>
+
+
+//                             {/* ------------Part-2 (Sub Part-1)------------------- */}
+//       <div>
+//           <h1  style={{textAlign: "center"}}>Problem - 2 (Method - 1)</h1>
+//           <div className="BtnColourDimention">
+//             <JoinUs />
+//             <Settings />
+//           </div>
+//           <div className="BtnColourDimention">
+//             <Login />
+//             <ContactUs />
+//           </div>
+//           <div className="BtnColourDimention">
+//             <Search />
+//             <Help />
+//           </div>
+//           <div className="BtnColourDimention">
+//             <Home />
+//             <Download />
+//           </div>
+//       </div>
+
+
+//                             {/* ------------Part-2 (Sub Part-2)------------------- */}
+//       <div>
+//             <h1  style={{textAlign: "center"}}>Problem - 1 (Method -  2)</h1>
+//             <div className='BtnColourDimention2'>
+//                 <PropsButtonMethod/>
+//             </div>
+//       </div>
 //     </div>
 //   );
 // }
 
 // export default App;
+
+
+
+
+
+//------------------------------------------- Day-2 Lecture Part (07/04/2022)-------------------------------------------------------------------//
+
+import './App.css';
+
+import Button from './day2/Button';
+import Employ from './day2/Employ';
+
+function App() {
+  return (
+    <div className="App">
+      <Button/>
+      <Employ/>
+    </div>
+  );
+}
+
+export default App;
--- src/day2/Button.jsx
+++ src/day2/Button.jsx
@@ -8,11 +8,15 @@ function Button() {
     const handleDec = () => {
         setCount(count - 1);
     }
+    const handleDouble = () => {
+        setCount(count*2);
+    }
     return(
         <>
         <h1 style={{color:"white"}}>{count}</h1>
             <button onClick={()=>handleInc()}>Increase</button>
             <button onClick={()=>handleDec()}>Decrease</button>
+            <button onClick={()=>handleDouble()}>Double</button>
         </>
     );
 }
