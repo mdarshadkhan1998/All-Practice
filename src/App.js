197a359dec23e430a216ac6734f9c4ef558d7dbd
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-04-08 07:18:21 +0530

    Day-1 Assignment Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-// --------------------------------------------Day-1 Assignment----------------------------------------------------------------//
+// --------------------------------------------Day-1 Assignment (06/04/2022)----------------------------------------------------------------//
 
 import './App.css';
-import ContactUs from './day1/buttons/ContactUs';
-import Download from './day1/buttons/Download';
-import Help from './day1/buttons/Help';
-import Home from './day1/buttons/Home';
-import JoinUs from './day1/buttons/JoinUs';
-import Login from './day1/buttons/Login';
-import Search from './day1/buttons/Search';
-import Settings from './day1/buttons/Settings';
-import Mobile from './day1/Mobile';
-import Tv from './day1/Tv';
+import ContactUs from './day1/Buttons-Part-1/ContactUs';
+import Download from './day1/Buttons-Part-1/Download';
+import Help from './day1/Buttons-Part-1/Help';
+import Home from './day1/Buttons-Part-1/Home';
+import JoinUs from './day1/Buttons-Part-1/JoinUs';
+import Login from './day1/Buttons-Part-1/Login';
+import Search from './day1/Buttons-Part-1/Search';
+import Settings from './day1/Buttons-Part-1/Settings';
+import PropsButtonMethod from './day1/Buttons-Part-2/PropsButtonMethod';
+import Mobile from './day1/Display-List-Problem-1/Mobile';
+import Tv from './day1/Display-List-Problem-1/Tv';
 
 function App() {
   return (
     <div>
+                                 {/* ------------Part-1------------------- */}
+      <h1  style={{textAlign: "center"}}>Problem - 1</h1>
       <div className="App">
         <Mobile/>
         <Tv/>
       </div>
+
+
+                            {/* ------------Part-2 (Sub Part-1)------------------- */}
       <div>
+          <h1  style={{textAlign: "center"}}>Problem - 2 (Method - 1)</h1>
           <div className="BtnColourDimention">
             <JoinUs />
             <Settings />
@@ -37,6 +44,15 @@ function App() {
             <Download />
           </div>
       </div>
+
+
+                            {/* ------------Part-2 (Sub Part-2)------------------- */}
+      <div>
+            <h1  style={{textAlign: "center"}}>Problem - 1 (Method -  2)</h1>
+            <div className='BtnColourDimention2'>
+                <PropsButtonMethod/>
+            </div>
+      </div>
     </div>
   );
 }
@@ -44,7 +60,7 @@ function App() {
 export default App;
 
 
-//------------------------------------------- Day-2 Lecture Part-------------------------------------------------------------------//
+//------------------------------------------- Day-2 Lecture Part (07/04/2022)-------------------------------------------------------------------//
 
 // import './App.css';
 
